18727880a6187511ba6d380a6e7f191f51eeb218
Cristian Camilo Gualteros García <cgualteros@choucairtesting.com>
2023-11-25 20:47:18 -0500

    agregamos el after con el quit

--- src/test/java/motoresdebusqueda/dogpile/CuandoBuscoResultadosConTexto.java
+++ src/test/java/motoresdebusqueda/dogpile/CuandoBuscoResultadosConTexto.java
@@ -1,12 +1,19 @@
 package motoresdebusqueda.dogpile;
 
 import motoresdebusqueda.dogpile.steps.UsuarioDogPile;
+import motoresdebusqueda.dogpile.utils.KillBrowser;
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.thucydides.core.annotations.Managed;
 import net.thucydides.core.annotations.Steps;
+import net.thucydides.core.webdriver.SerenityWebdriverManager;
+import net.thucydides.core.webdriver.WebdriverManager;
+import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
+
+import java.io.IOException;
+import java.util.List;
 
 @RunWith(SerenityRunner.class)
 
@@ -25,5 +32,13 @@ public class CuandoBuscoResultadosConTexto {
 
         juan.deberia_ver_resultado_de_titulo_con_texto_correcto("Argentine rock - Wikipedia",
                 "Argentine Rock Nacional is one of the principal predecessors");
+    }
+
+    @After
+    public void despuesDeEjecutarElTest(){
+
+        driver.quit();
+        //cerrartodoslo;
+        
     }
 }
